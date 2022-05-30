b60ff23a9d5cb7f9468f8106b6401defa52a3165
Sanaz <snz5seifi@gmail.com>
2022-05-30 03:07:12 -0400

    ExtraHours change every month so it should be a parameter!

--- src/main/java/SalaryProjct/Employee.java
+++ src/main/java/SalaryProjct/Employee.java
@@ -2,6 +2,9 @@ package SalaryProjct;
 
 public class Employee {
     public int baseSalary;
-    public int extraHours;
     public int hourlyRate;
+
+public int calculateWage(int extraHours) {
+    return baseSalary + (extraHours * hourlyRate);
+}
 }
