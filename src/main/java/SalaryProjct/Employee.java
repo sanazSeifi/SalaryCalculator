ff925cf050560fb7dced549093e851bd651ee30b
Sanaz <snz5seifi@gmail.com>
2022-05-30 03:23:33 -0400

    Adding setBaseSalary because we change access modifier to private

--- src/main/java/SalaryProjct/Employee.java
+++ src/main/java/SalaryProjct/Employee.java
@@ -1,10 +1,16 @@
 package SalaryProjct;
 
 public class Employee {
-    public int baseSalary;
+    private int baseSalary;
     public int hourlyRate;
 
 public int calculateWage(int extraHours) {
     return baseSalary + (extraHours * hourlyRate);
+}
+
+public void setBaseSalary(int baseSalary){
+    if(baseSalary <= 0){
+        throw
+    }
 }
 }
